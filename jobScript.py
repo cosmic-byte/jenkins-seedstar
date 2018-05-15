6f43c7e460dc6bf8ac9e7604475927864c04bed1
gregobinna <gregobinna@gmail.com>
2018-05-15 10:31:06 +0100

    configure jenkins server

--- jobScript.py
+++ jobScript.py
@@ -2,11 +2,13 @@ import jenkins
 from config import Config
 
 
-# server = jenkins.Jenkins('http://localhost:3000', username='greg', password='mypassword')
-# user = server.get_whoami()
-# version = server.get_version()
-# print('Hello %s from Jenkins %s' % (user['fullName'], version))
+def start():
+    db = Config()
+    db.start()
+    server = jenkins.Jenkins('http://localhost:8080', username='admin', password='e60fae9cf43e45cba8faa33c3ff746c9')
+    user = server.get_whoami()
+    version = server.get_version()
+    print('Hello %s from Jenkins %s' % (user['fullName'], version))
 
 if __name__ == '__main__':
-       db = Config()
-       db.start()
+       start()
